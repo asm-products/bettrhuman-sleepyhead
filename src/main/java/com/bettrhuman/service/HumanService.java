c4c41467d3a41ca238c8a69753fd8eb7f96301b1
tshields <troy.leland.shields@gmail.com>
2014-09-29 08:26:26 -0600

    Adding implementation of human service.

---
+++ src/main/java/com/bettrhuman/service/HumanService.java
@@ -0,0 +1,13 @@
+package com.bettrhuman.service;
+
+import com.bettrhuman.service.models.Human;
+
+public class HumanService implements IHumanService {
+
+	@Override
+	public Human login(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
